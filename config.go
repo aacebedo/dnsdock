9c99d81e3a06542f956e79641a33f8e5cadfe8a1
Karl Gutwin <karl.gutwin@biogen.com>
2015-07-05 09:22:02 -0400

    Enable Docker TLS

--- config.go
+++ config.go
@@ -27,6 +27,10 @@ type Config struct {
 	dnsAddr    string
 	domain     Domain
 	dockerHost string
+	tlsVerify  bool
+	tlsCaCert  string
+	tlsCert    string
+	tlsKey     string
 	verbose    bool
 	httpAddr   string
 	ttl        int
@@ -37,6 +41,11 @@ func NewConfig() *Config {
 	if len(dockerHost) == 0 {
 		dockerHost = "unix:///var/run/docker.sock"
 	}
+	tlsVerify := len(os.Getenv("DOCKER_TLS_VERIFY")) != 0
+	dockerCerts := os.Getenv("DOCKER_CERT_PATH")
+	if len(dockerCerts) == 0 {
+		dockerCerts = os.Getenv("HOME") + "/.docker"
+	}
 
 	return &Config{
 		nameserver: "8.8.8.8:53",
@@ -44,6 +53,10 @@ func NewConfig() *Config {
 		domain:     NewDomain("docker"),
 		dockerHost: dockerHost,
 		httpAddr:   ":80",
+		tlsVerify:  tlsVerify,
+		tlsCaCert:  dockerCerts + "/ca.pem",
+		tlsCert:    dockerCerts + "/cert.pem",
+		tlsKey:     dockerCerts + "/key.pem",
 	}
 
 }
