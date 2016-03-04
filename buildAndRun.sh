cca7cb638b79dc12380a31037c6d8c57f6076182
Nelson Graça <nelsongraca@users.noreply.github.com>
2016-03-04 01:41:11 +0000

    revert for pull request

--- buildAndRun.sh
+++
@@ -1,4 +0,0 @@
-docker stop dnsdock
-docker rm dnsdock
-docker build -t tonistiigi/dnsdock .
-docker run -d -p "80:80" -p "53:53/udp" --name dnsdock -v /var/run/docker.sock:/var/run/docker.sock tonistiigi/dnsdock
