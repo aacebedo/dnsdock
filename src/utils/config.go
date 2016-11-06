faf3fb4fe6db43c1d306e0a336f2ab736e6ab76f
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-11-06 13:53:47 +0100

    Applied MIT license to all files

--- src/utils/config.go
+++ src/utils/config.go
@@ -1,3 +1,11 @@
+/* config.go
+ *
+ * Copyright (C) 2016 Alexandre ACEBEDO
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 package utils
 
 import (
